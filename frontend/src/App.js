d5aa661462e75f624ecc8f9cc362ae37641294f3
arusafa <arusafacollege@gmail.com>
2023-01-24 01:50:16 -0500

    fronend

---
+++ frontend/src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+// Front End //
+
+function App() {
+  return (
+    <div>
+
+      <div class="block">
+        <div className="header">
+          <p1>Online Tutorial Web Application</p1>
+        </div>
+      </div>
+      <div className='mainx'>
+
+        Web app that has features to promote
+        collaboration between. 3 main features would be,
+        video group chat, resources for learning, person blog
+        for showing personal achievements and to find friends
+        that like similar things, Whiteboard that helps the
+        session with tutor by giving
+        them tools for communication of concepts.
+      </div>
+
+      <div className='link_wrapper'>
+
+        <a href='#'>About</a>
+        <a href='#'>Features</a>
+        <a href='#'>Price</a>
+        <a href='#'>Benefits</a>
+        <a href='#'>Become Tutor</a>
+        <a href='#'>Register</a>
+        <a href='#'>Login</a>
+      </div>
+
+    </div>
+  )
+}
+
+export default App;
+
+
+
+
+// return (
+//   <div className='container'>
+//    {/* <div className='ring'></div>
+//    <div className='ring'></div>
+//    <div className='ring'></div> */}
+//    {/* <p>Loading.....</p> */}
+//    <div className='box'> </div>
+
+//   </div>
+//  );
